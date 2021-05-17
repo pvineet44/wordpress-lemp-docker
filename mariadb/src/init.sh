4dc91958ef79676a3f2e961567830f1b05437af4
Vineet Parekh <pvineet44@gmail.com>
2021-05-17 20:16:52 +0530

    domain

--- mariadb/src/init.sh
+++ mariadb/src/init.sh
@@ -5,7 +5,7 @@ echo "CREATE USER 'user'@'%' IDENTIFIED BY 'password';" | mysql -u root
 echo "GRANT ALL PRIVILEGES ON *.* TO 'user'@'%' WITH GRANT OPTION;" | mysql -u root
 echo "FLUSH PRIVILEGES;" | mysql -u root
 
-echo "GRANT ALL PRIVILEGES ON *.* TO 'vparekh'@'localhost' IDENTIFIED BY 'password';" | mysql -u     root
+echo "GRANT ALL PRIVILEGES ON *.* TO 'vparekh'@'%' IDENTIFIED BY 'password';" | mysql -u     root
 echo "FLUSH PRIVILEGES;" | mysql -u root
 echo "CREATE DATABASE ft_server;" | mysql -u vparekh -ppassword
 mysql -u vparekh -ppassword ft_server < /home/root/ft_server.sql
