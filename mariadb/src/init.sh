44d88ec80e6c8e781cccb19a1750014cac4c929f
Mohit <mohitashar1995@gmail.com>
2021-05-18 17:21:21 +0530

    init script error

--- mariadb/src/init.sh
+++ mariadb/src/init.sh
@@ -1,6 +1,6 @@
 echo "=== Starting MariaDB Service ==="
 
-PASS=$(sh ./decode.sh U2FsdGVkX1+OXnQmiCHyB+p0gLv8bnNkJlKl8NHEtag=)
+PASS=$(sh /home/root/decode.sh U2FsdGVkX1+OXnQmiCHyB+p0gLv8bnNkJlKl8NHEtag=)
 
 
 service mysql start
@@ -8,18 +8,11 @@ service mysql start
 
 # echo "CREATE USER 'user'@'%' IDENTIFIED BY '$PASS';" | mysql -u root
 echo "GRANT ALL PRIVILEGES ON *.* TO 'user'@'%' IDENTIFIED BY '$PASS';" | mysql -u root
-touch 1
 echo "FLUSH PRIVILEGES;" | mysql -u root
-touch 2
-
 
 echo "GRANT ALL PRIVILEGES ON *.* TO 'vparekh'@'localhost' IDENTIFIED BY '$PASS';" | mysql -u     root
-touch 3
 echo "FLUSH PRIVILEGES;" | mysql -u root
-touch 4
 echo "CREATE DATABASE ft_server;" | mysql -u vparekh -p$PASS
-touch 5
 mysql -u vparekh -p$PASS ft_server < /home/root/ft_server.sql
-touch 6
 
 exec "$@"
