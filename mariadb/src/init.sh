e9e02992a2db7996fc7ea728d93498fe07871051
Vineet Parekh <pvineet44@gmail.com>
2021-05-18 11:52:01 +0530

    db script change

--- mariadb/src/init.sh
+++ mariadb/src/init.sh
@@ -9,7 +9,7 @@ service mysql start
 echo "CREATE USER 'user'@'%' IDENTIFIED BY 'password';" | mysql -u root
 echo "GRANT ALL PRIVILEGES ON *.* TO 'user'@'%' WITH GRANT OPTION;" | mysql -u root
 echo "FLUSH PRIVILEGES;" | mysql -u root
-echo "CREATE DATABASE ft_server;" | mysql -u user -ppassword
+# echo "CREATE DATABASE ft_server;" | mysql -u user -ppassword
 # mysql -u user -p$PASS ft_server < /home/root/ft_server.sql
 
 echo "GRANT ALL PRIVILEGES ON *.* TO 'vparekh'@'localhost' IDENTIFIED BY 'password';" | mysql -u     root
