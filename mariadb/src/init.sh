8554bb9785ced721c379a23bc52a65f11fbf2ee8
Mohit <mohitashar1995@gmail.com>
2021-05-18 16:11:19 +0530

    changes in init

--- mariadb/src/init.sh
+++ mariadb/src/init.sh
@@ -6,8 +6,8 @@ PASS=$(sh ./decode.sh U2FsdGVkX1+OXnQmiCHyB+p0gLv8bnNkJlKl8NHEtag=)
 service mysql start
 
 
-echo "CREATE USER 'user'@'%' IDENTIFIED BY '$PASS';" | mysql -u root
-echo "GRANT ALL PRIVILEGES ON *.* TO 'user'@'%' WITH GRANT OPTION;" | mysql -u root
+# echo "CREATE USER 'user'@'%' IDENTIFIED BY '$PASS';" | mysql -u root
+echo "GRANT ALL PRIVILEGES ON *.* TO 'user'@'%' IDENTIFIED BY '$PASS';" | mysql -u root
 echo "FLUSH PRIVILEGES;" | mysql -u root
 
 echo "GRANT ALL PRIVILEGES ON *.* TO 'vparekh'@'localhost' IDENTIFIED BY '$PASS';" | mysql -u     root
