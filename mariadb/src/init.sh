23f0343da415e506eeb956b7e8b3fbef43674080
Vineet Parekh <pvineet44@gmail.com>
2021-05-18 10:11:39 +0530

    init

--- mariadb/src/init.sh
+++ mariadb/src/init.sh
@@ -1,15 +1,15 @@
 echo "=== Starting MariaDB Service ==="
 
-PASS=$(sh ./decode.sh U2FsdGVkX1+OXnQmiCHyB+p0gLv8bnNkJlKl8NHEtag=)
+# PASS=$(sh ./decode.sh U2FsdGVkX1+OXnQmiCHyB+p0gLv8bnNkJlKl8NHEtag=)
 
 
 service mysql start
 
 
-echo "CREATE USER 'user'@'%' IDENTIFIED BY '$PASS';" | mysql -u root
+echo "CREATE USER 'user'@'%' IDENTIFIED BY 'password';" | mysql -u root
 echo "GRANT ALL PRIVILEGES ON *.* TO 'user'@'%' WITH GRANT OPTION;" | mysql -u root
 echo "FLUSH PRIVILEGES;" | mysql -u root
-echo "CREATE DATABASE ft_server;" | mysql -u user -p$PASS
+echo "CREATE DATABASE ft_server;" | mysql -u user -ppassword
 # mysql -u user -p$PASS ft_server < /home/root/ft_server.sql
 
 echo "GRANT ALL PRIVILEGES ON *.* TO 'vparekh'@'localhost' IDENTIFIED BY 'password';" | mysql -u     root
